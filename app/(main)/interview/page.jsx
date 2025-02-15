38442816d61ab16c7a02957a525e68eff2c6f6e9
smit jogani <smitjogani00@gmail.com>
2025-02-15 14:12:38 +0530

    Mock Interview Performance Dashboard

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,8 +1,27 @@
+import { getAssessments } from '@/actions/interview'
 import React from 'react'
+import StatsCard from './_components/stats-card';
+import PerformaceChart from './_components/performace-chart';
+import QuizList from './_components/quiz-list';
 
-const InterviewPage = () => {
+const InterviewPage = async () => {
+
+  const assessments = await getAssessments();
+
   return (
-    <div>InterviewPage</div>
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+
+      <div className="space-y-6">
+        <StatsCard assessments={assessments} />
+        <PerformaceChart assessments={assessments}/>
+        <QuizList assessments={assessments}/>
+      </div>
+    </div>
   )
 }
 
